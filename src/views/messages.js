309537340f4d4a075ae56d44b2b269b311205d04
zhangxinjun <zhangxinjun@9orange.com>
2020-09-08 17:09:26 +0800

    路由传参

--- src/views/messages.js
+++ src/views/messages.js
@@ -1,28 +1,40 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 
+import MessageDetail from "./message-detail"
+import { NavLink, Switch, Route } from "react-router-dom"
 export default class Message extends Component {
     state = {
-        messages :[]
+        messages: []
     }
 
-    componentDidMount () {
+    componentDidMount() {
         setTimeout(() => {
-            const  messages = [{id:1,title:"messages1"},{id:2,title:"messages2"},{id:3,title:"messages3"}];
-            this.setState({messages})
-        },1000)
+            const messages = [
+                { id: 1, title: "messages1" },
+                { id: 2, title: "messages2" },
+                { id: 3, title: "messages3" }
+            ];
+            this.setState({ messages })
+        }, 1000)
     }
 
-    render () {
+    render() {
         return (
-            <ul>
-                {
-                   this.state. messages.map((item,index) => (
-                        <li>
-                            <a href="#">{item.title}</a>
-                        </li>
-                    ))
-                }
-            </ul>
+            <div>
+                <ul>
+                    {
+                        this.state.messages.map((item, index) => (
+                            <li key={index}>
+                                <a href={`/home/message/${item.id}`}>{item.title}</a>
+                            </li>
+                        ))
+                    }
+
+                </ul>
+                <Route path="/home/message/:id" component={MessageDetail}></Route>
+
+            </div>
+
         )
     }
 }
