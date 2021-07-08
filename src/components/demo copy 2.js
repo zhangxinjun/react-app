adf7ba61431414d47b0175befc17ae5a90e2ce6c
zhangxinjun <xinjun.zhang@i-driven.com.cn>
2021-07-08 15:06:30 +0800

    demo

--- src/components/demo copy 2.js
+++ src/components/demo copy 2.js
@@ -2,8 +2,7 @@ import React from 'react'
 export default class Demo extends React.Component {
   state = {
     name: '',
-    pwd: '',
-    opticty: 1
+    pwd: ''
   }
   // 返回一个函数交给onChange事件调用dataType是onChange传递过来的参数
   formDate = (dataType) => {
@@ -12,25 +11,10 @@ export default class Demo extends React.Component {
       this.setState({ [dataType]: event.target.value })
     }
   }
-  componentDidMount () {
-    this.time = setInterval(() => {
-      let { opticty } = this.state
-      opticty -= 0.1
-      if (opticty < 0) {
-        opticty = 1
-      }
-      this.setState({ opticty })
-    }, 200)
-  }
-  unmoun = () => {
-    clearInterval(this.time)
-  }
   render () {
-    const { name, pwd, opticty } = this.state
-    console.log("+++");
+    const { name, pwd } = this.state
     return (
       <div>
-        <h1 style={{ opacity: opticty }}>标题</h1>
         <form>
           {/* onChange事件接受的是formDate函数的返回值 */}
           <input type="text" onChange={this.formDate('name')} />
@@ -38,7 +22,6 @@ export default class Demo extends React.Component {
           <input type="text" onChange={this.formDate('pwd')} />
         </form>
         <div>输入的用户名：{name}密码：{pwd}</div>
-        <button onClick={this.unmoun}>卸载</button>
       </div>
     )
   }
