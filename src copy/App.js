4f26b6dfa062b6ed53eb24bf2245655ca67adba4
zhangxinjun <xinjun.zhang@i-driven.com.cn>
2021-07-16 14:32:33 +0800

    init

--- src copy/App.js
+++ src copy/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
+import store from './redux/store'
 import 'antd/dist/antd.css';
+// 引入容器组件并传递store
+import Count from './container/CountUi'
 
 
 export default class App extends Component {
   render () {
     return (
+
       <div>
-        app
+        <Count store={store} />
       </div>
     )
   }
